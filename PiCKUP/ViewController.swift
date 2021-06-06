c35917b365b6b08a3d932f0dce56a607252d846d
RevealSquid <hubertle43100@gmail.com>
2021-06-06 09:38:29 -0700

    Initial Commit

---
+++ PiCKUP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiCKUP
+//
+//  Created by Hubert Le on 6/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
